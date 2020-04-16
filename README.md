ed6660d630508138992843bf2d09604affdf628c
qlhan <qlhan@incarcloud.com>
2020-04-16 20:27:04 +0800

    0.2.0 rpx and vw

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 ### 初始化项目
 
 ```node
-vue create -p dcloudio/uni-preset-vue uniapp-achieve
+vue create -p dcloudio/uni-preset-vue uniapp-achieve2
 ```
 
 ### 运行微信小程序项目
@@ -20,6 +20,24 @@ npm run dev:mp-weixin
 
 ```node
 npm run build:mp-weixin
+```
+
+## 相关配置
+
+### 支持sass语法
+
+```node
+npm i node-sass sass-loader -D
+```
+
+```html
+<style lang="scss">
+	.content {
+		.sass {
+			background-color: $uni-color-primary;
+		}
+	}
+</style>
 ```
 
 ## 附录：用户自定义配置
---
+++ src/pages/sample-01/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="content">
+		<view class="rpx">rpx</view>
+		<view class="vw">vw</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello xx!'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.rpx {
+		/* 小程序单位 -> screen width = 750rpx */
+		width: 750rpx;
+		height: 100px;
+		background-color: red;
+	}
+	.vw {
+		/* h5单位 -> screen width = 100vw, screen height = 100vw */
+		width: 50vw;
+		height: 25vw;
+		background-color: orange;
+	}
+</style>
