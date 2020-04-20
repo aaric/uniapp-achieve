eca357f56ba95bb342acc51f2e59c929ddb918f7
qlhan <qlhan@incarcloud.com>
2020-04-20 20:21:46 +0800

    0.6.0 components

---
+++ src/components/ImgLoader.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<image @click="handleClick" class="img" :src="src"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'ImgLoader',
+		props: {
+			src: String
+		},
+		data() {
+			return {
+				msg: 'Hi Parent!'
+			}
+		},
+		methods: {
+			handleClick() {
+				this.$emit('clickBy', this.src);
+			}
+		}
+	}
+</script>
+
+<style>
+  .img {
+		width: 100px;
+		height: 100px;
+	}
+</style>
---
+++ src/pages/sample-05/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="content">
+		<img-loader @clickBy='handleChildClick' src="http://www.easyicon.net/api/resizeApi.php?id=1174114&size=128"></img-loader>
+		<img-loader @clickBy='handleChildClick' src="http://www.easyicon.net/api/resizeApi.php?id=1173679&size=128"></img-loader>
+	</view>
+</template>
+
+<script>
+	import ImgLoader from '@/components/ImgLoader.vue';
+	
+	export default {
+		components: {
+			ImgLoader
+		},
+		methods: {
+			handleChildClick(msg) {
+				console.log(msg);
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+</style>
