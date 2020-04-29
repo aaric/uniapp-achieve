99d65e29061144307cd4e653443714ec5a98894b
qlhan <qlhan@incarcloud.com>
2020-04-29 19:40:49 +0800

    1.2.0 uni-ui

--- src/pages/navi-home/index.vue
+++ src/pages/navi-home/index.vue
@@ -1,15 +1,31 @@
 <template>
 	<view class="content">
 		<text class="iconfont icon-chat"></text>
-		首页
+		<view class="sass">
+			首页
+		</view>
+		<view>
+			<uni-badge text="1"></uni-badge>
+			<uni-badge text="2" type="success"></uni-badge>
+			<uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+		</view>
 	</view>
 </template>
 
 <script>
+	import {uniBadge} from '@dcloudio/uni-ui'
+	
 	export default {
-		
+		components: {
+			uniBadge
+		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.content {
+		.sass {
+			background-color: $uni-color-primary;
+		}
+	}
 </style>
